5ff4a195afd170a2a90b54c7d024fca6b461b72e
vanessacamargo1606 <vanessa.camargo@uao.edu.co>
2024-05-29 19:07:38 -0500

    Actualizacion

--- src/db/users.js
+++ src/db/users.js
@@ -2,42 +2,20 @@ import { addDoc, doc, collection, getDocs, deleteDoc, updateDoc, query, getDoc }
 import { db } from "../core/service/firebase/firebase";
 
 
-//  async function readUsers(db) {
-//    const querySnapshot = await getDocs(collection(db, "users"));
-//    let data = querySnapshot.docs;
-//    let response = querySnapshot.docs.map((doc) => doc.data());
-//    return response;
-//  }
-
-// async function addUser(db) {
-//   try {
-//     const docRef = await addDoc(collection(db, "users"), {
-//       first: "Alan",
-//       middle: "Mathison",
-//       last: "Turing",
-//       born: 1912,
-//     });
-
-//     console.log("Document written with ID: ", docRef.id);
-//   } catch (e) {
-//     console.error("Error adding document: ", e);
-//   }
-// }
-
 const saveTorneo = (name, fecha, cantidad, url) => {
   addDoc(collection(db, "Torneos"), { name, fecha, cantidad, url });
 }
 
-const getTorneo = async ()  => {
- const result = await getDocs(query(collection(db, "Torneos")));
- return result;
+const getTorneo = async () => {
+  const result = await getDocs(query(collection(db, "Torneos")));
+  return result;
 }
 
 const deleteTorneo = async (id) => { // El id del que voy a borrar
   await deleteDoc(doc(db, "Torneos", id));
 }
 
-const updateTorneo = async (id, name, fecha, cantidad,url) => { // El id que quiero actualizar y su nuevo nombre
+const updateTorneo = async (id, name, fecha, cantidad, url) => { // El id que quiero actualizar y su nuevo nombre
   await updateDoc(doc(db, "Torneos", id), { name, fecha, cantidad, url })
 }
 
